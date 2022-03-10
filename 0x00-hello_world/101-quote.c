c3eb43d3a65f4fba17968cf0a449d31bdd7d51a3
MungaiVic <victornmungai@gmail.com>
2022-03-10 17:01:01 +0300

    Final: stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
   * main - Prints some text then new line to the standard error
@@ -7,8 +6,6 @@
 
 int main(void)
 {
-	/* write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);*/
-	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, " - Dora Korpar, 2015-10-19\n")
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
