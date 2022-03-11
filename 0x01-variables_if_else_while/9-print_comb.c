5af4c5d807b6fbe71e0067c04aa59751db603064
MungaiVic <victornmungai@gmail.com>
2022-03-11 13:23:06 +0300

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - Prints single digit nums then new line
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		putchar(c);
+		if(c != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
