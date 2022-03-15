5397a2f2efc93c0afc859c1f7a918714b24b734c
MungaiVic <victornmungai@gmail.com>
2022-03-15 13:55:02 +0300

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * jack_bauer - Prints time from 00:00 to 23:59
+  * Return: Always 0 (Successful)
+  *
+  */
+
+void jack_bauer(void)
+{
+	int hh;
+	int mm;
+
+	while(hh < 24)
+	{
+		mm = 0;
+		while (mm < 60)
+		{
+			_putchar(hh + '0');
+			_putchar(':');
+			_putchar(mm + '0');
+			_putchar('\n');
+			mm++;
+		}
+	}
+}
