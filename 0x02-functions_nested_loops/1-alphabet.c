7df9d92b8e5c6520f3054d5d8e34f94d7a9e6e75
MungaiVic <victornmungai@gmail.com>
2022-03-15 11:05:06 +0300

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+#include "main.h"
+
+/*
+ * main - prints out the alphabet
+ * Return: Always 0 (Successful)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <=122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
