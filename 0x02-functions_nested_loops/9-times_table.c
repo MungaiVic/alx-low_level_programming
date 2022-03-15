a38e7fafa23fdf84c19325ae2f3ea6ee6697922f
MungaiVic <victornmungai@gmail.com>
2022-03-15 14:21:22 +0300

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+  * times_table - prints the multiplication table
+  * Return: Always 0 (Successful)
+  */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k = 9;
+
+	for (i = 0; i <= k; i++)
+	{
+		for (j = 0; j <= k; j++)
+		{
+			if ((i * j) < 10)
+			{
+				if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar((j * i) + '0');
+			}
+			else if ((i * j) < 100)
+			{
+				_putchar(' ');
+				_putchar((((j * i) / 10) % 10) + '0');
+				_putchar(((j * i) % 10) + '0');
+			}
+			if (j != k)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
