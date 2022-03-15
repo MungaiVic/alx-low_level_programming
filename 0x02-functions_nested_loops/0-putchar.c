b1d853246f27e3bb74a29c35239f0428f3248ee5
MungaiVic <victornmungai@gmail.com>
2022-03-15 09:59:30 +0300

    Add gitignore

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* 
+ * main - prints "_putchar"
+ *
+ * Return: Always 0 (Successful)
+ */
+
+int main(void)
+{
+	char ch = "_putchar";
+
+	putchar((int)ch);
+
+	return (0);
+}
