3c0171652ddf3343747187a484bca4f393a0fae7
MungaiVic <victornmungai@gmail.com>
2022-03-15 12:27:05 +0300

    Fix: 5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 
 /**
   * print_sign - Prints output relationship to 0
@@ -9,17 +8,17 @@
 
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('+');
+		return (1);
 	} else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	} else if (n > 0)
+	} else
 	{
-		_putchar('+');
-		return (1);
+		_putchar('-');
+		return (-1);
 	}
 }
