d02ee42c7f86a28f3725428013ddd18b53273f6e
MungaiVic <victornmungai@gmail.com>
2022-03-18 17:51:03 +0300

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ *
+ * @a: is a param
+ * @b: is a param
+ *
+ * Return: Always 0.
+ */
+
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
