2fddabb52c01e46a192c261f83230424e6d9371c
MungaiVic <victornmungai@gmail.com>
2022-03-21 17:16:00 +0300

    string lengths

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _strlen - Returns length of string
+  * @s: pointer to string to be evaluated
+  * Return: length of string
+  */
+
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0)
+	{
+		count++;
+		s++;
+	}
+}
