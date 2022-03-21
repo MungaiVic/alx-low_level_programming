0e05ff1650fe01f9e9cd4bb0672ae6ec2bd5283e
MungaiVic <victornmungai@gmail.com>
2022-03-21 17:26:00 +0300

    print to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts - Prints a string to stdout
+  * @str: string to be printed
+  * Return: void
+  */
+
+void _puts(char *str)
+{
+	while(*str != '\0')
+	{
+		printf("%c", *str);
+		str++;
+	}
+	printf("\n");
+}
