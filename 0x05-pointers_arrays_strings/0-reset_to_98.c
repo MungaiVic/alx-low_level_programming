c4797b6f302510b25a29db444cf59f9e8ac0ba56
MungaiVic <victornmungai@gmail.com>
2022-03-21 16:47:57 +0300

    Update to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-  * swap_int - Updates parameter to 98
+  * reset_to_98 - Updates parameter to 98
   * @n : pointer parameter
   * Return: void
   */
 
-void swap_int(int *n)
+void reset_to_98(int *n)
 {
 	n = 98;
 }
