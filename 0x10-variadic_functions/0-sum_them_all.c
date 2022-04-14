25129d704e4fb8ec5dca8629ec6aefb24f915036
MungaiVic <victornmungai@gmail.com>
2022-04-14 21:28:50 +0300

    sum them all fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,20 +7,19 @@
 * @n: n args
 * Return: sum or if n == 0, return 0
 */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
 	unsigned int i;
+	int sum = 0;
 	va_list list;
 
-	if (n)
-	{
-		va_start(list, n);
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(list, int);
-		}
-		va_end(list);
-	}
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
+
+	va_end(list);
+
 	return (sum);
 }
